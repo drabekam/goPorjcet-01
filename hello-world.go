220f636a6db681a55b11a3d5307064e89a2c7465
drabekam <drabekam@mail.uc.edu>
2024-05-16 22:46:45 -0400

    Hello-World-GoLang

--- hello-world.go
+++
@@ -1,9 +0,0 @@
-// creating hellow world program to get started with go lang
-
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hellow world")
-}
---
+++ src/hello-world.go
@@ -0,0 +1,9 @@
+// creating hellow world program to get started with go lang
+
+package main // A package is a collection of file that can be resued. Package main is special as this is a entry for the application.
+
+import "fmt" // import packages or libraries
+
+func main() { // Every app needs main function even if its a web app or whatever it needs a main function as the entry point.
+	fmt.Println("Hellow world")
+}
